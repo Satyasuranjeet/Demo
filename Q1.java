3b2533c62d588751e3be3b6a14ec86fc905a8d79
satyasuranjeet <satyajena911@gmail.com>
2023-10-08 00:41:54 +0530

    Added example

---
+++ Q1.java
@@ -0,0 +1,26 @@
+package com.company;
+
+class lopa{
+    int k,j;
+    void getdata(int p,int q) {
+        k = p;
+        j = q;
+    }
+    int sum() {
+        return k + j;
+    }
+    int product(){
+        return j*k;
+
+
+    }
+
+}
+public class Q1{
+    public static void main(String args[]){
+        lopa a=new lopa();
+        a.getdata(5,9);
+        System.out.println(a.sum());
+        System.out.println(a.product());
+    }
+}
